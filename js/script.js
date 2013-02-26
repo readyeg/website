4a3b4a445c9b2bdf69bff6d93b58aa6e03b76c0d
Karan Dhindsa <karan@readyedgego.com>
2013-02-26 01:11:44 -0800

    Added mouseover functionality to navigation bar. When mouse is hovered over a menu, it shows a drop down.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,24 @@
 $(document).ready(function(){
 	console.log('jquery loaded.');
 
-	$("#horizontal-tabs").tabs({
-		collapsible: true
+	$('#horizontal-tabs ul li').mouseover(function(){
+
+		var $id = $(this).attr('id');
+		var $contentDiv = $('#' + $id + '_content');
+
+		$contentDiv
+			.removeClass('horizontal-tab-content-hide')
+			.addClass('horizontal-tab-content-show');
+	});
+
+	$('#horizontal-tabs ul li').mouseout(function(){
+
+		var $id = $(this).attr('id');
+		var $contentDiv = $('#' + $id + '_content');
+
+		$contentDiv
+			.removeClass('horizontal-tab-content-show')
+			.addClass('horizontal-tab-content-hide');
 	});
 });
 
