6cf9d1c8c648cc3505c0bd950bda3d8c08501199
Karan Dhindsa <karan@readyedgego.com>
2013-02-27 00:03:36 -0800

    All drop down menus working over hovering.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 $(document).ready(function(){
 	console.log('jquery loaded.');
 
-	$('#horizontal-tabs ul li').mouseover(function(){
+	var tabTimeout;
+
+	$('#horizontal-tabs ul li').mouseenter(function(){
 
 		var $id = $(this).attr('id');
 		var $contentDiv = $('#' + $id + '_content');
@@ -12,14 +14,70 @@ $(document).ready(function(){
 			.addClass('horizontal-tab-content-show');
 	});
 
-	$('#horizontal-tabs ul li').mouseout(function(){
+	$('#horizontal-tabs ul li').mouseleave(function(){
 
 		var $id = $(this).attr('id');
 		var $contentDiv = $('#' + $id + '_content');
 
-		$contentDiv
+		tabTimeout = setTimeout(
+			function(){
+				$contentDiv
+					.removeClass('horizontal-tab-content-show')
+					.addClass('horizontal-tab-content-hide');
+				}, 
+			300);
+	});
+
+
+	$('#about_us_content').mouseenter(function(){
+		clearTimeout(tabTimeout);
+	});
+
+	$('#about_us_content').mouseleave(function(){
+		$(this)
 			.removeClass('horizontal-tab-content-show')
 			.addClass('horizontal-tab-content-hide');
 	});
+
+	$('#college_applications_content').mouseenter(function(){
+		clearTimeout(tabTimeout);
+	});
+
+	$('#college_applications_content').mouseleave(function(){
+		$(this)
+			.removeClass('horizontal-tab-content-show')
+			.addClass('horizontal-tab-content-hide');
+	});
+
+	$('#early_guidance_content').mouseenter(function(){
+		clearTimeout(tabTimeout);
+	});
+
+	$('#early_guidance_content').mouseleave(function(){
+		$(this)
+			.removeClass('horizontal-tab-content-show')
+			.addClass('horizontal-tab-content-hide');
+	});
+
+	$('#success_stories_content').mouseenter(function(){
+		clearTimeout(tabTimeout);
+	});
+
+	$('#success_stories_content').mouseleave(function(){
+		$(this)
+			.removeClass('horizontal-tab-content-show')
+			.addClass('horizontal-tab-content-hide');
+	});
+
+	$('#admission_resources_content').mouseenter(function(){
+		clearTimeout(tabTimeout);
+	});
+
+	$('#admission_resources_content').mouseleave(function(){
+		$(this)
+			.removeClass('horizontal-tab-content-show')
+			.addClass('horizontal-tab-content-hide');
+	});
+
 });
 
