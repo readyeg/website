68701e792cba05ffb61b2c6323f80d794856d485
Karan Dhindsa <karan@readyedgego.com>
2013-03-02 16:44:01 -0800

    Added nav bar background image. Dynamic indentation of nav bar content.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 	console.log('jquery loaded.');
 
 	var tabTimeout;
+	var navButtonPressColor = '#E0642f';
+	var $activeNavButton;
 
 	$('#horizontal-tabs ul li').mouseenter(function(){
 
@@ -10,8 +12,11 @@ $(document).ready(function(){
 		var $contentDiv = $('#' + $id + '_content');
 
 		var $docWidth = $(document).width();
-		var leftMargin = 0.17 * $docWidth;			// Left margin is 17% of page width
-		var contentDivWidth = 0.60 * $docWidth;		// Total width is 60% of page width
+		var leftMargin = $(this).offset().left - 6;			
+		var contentDivWidth = 0.20 * $docWidth;		
+
+		$activeNavButton = $(this);
+		$activeNavButton.css('background-color', navButtonPressColor);
 
 		$contentDiv
 			.removeClass('hide')
@@ -25,21 +30,24 @@ $(document).ready(function(){
 		var $id = $(this).attr('id');
 		var $contentDiv = $('#' + $id + '_content');
 
+		$activeNavButton.css('background-color', '');
+
 		tabTimeout = setTimeout(
 			function(){
 				$contentDiv
 					.removeClass('horizontal-tab-content-show')
 					.addClass('hide');
 				}, 
-			300);
+			10);
 	});
-
 	
 	$('#horizontal-tabs #contents div').mouseenter(function(){
+		$activeNavButton.css('background-color', navButtonPressColor);
 		clearTimeout(tabTimeout);
 	});
 
 	$('#horizontal-tabs #contents div').mouseleave(function(){
+		$activeNavButton.css('background-color', '');
 		$(this)
 			.removeClass('horizontal-tab-content-show')
 			.addClass('hide');
